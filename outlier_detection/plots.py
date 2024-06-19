d2334e6e207188488e8c63dfb9b8c5daa9b27a3e
brunodifranco <brunodifranco99@gmail.com>
2024-06-19 18:17:54 -0300

    UPDATE: add df

--- outlier_detection/plots.py
+++ outlier_detection/plots.py
@@ -1,15 +1,28 @@
+from typing import Optional
 import plotly.express as px
 from pandas.core.frame import DataFrame
 
 
 def time_series_plot(
-    df: DataFrame, target_col: str, title: str, width: int = 1200, height: int = 500
+    df: DataFrame,
+    target_col: str,
+    title: Optional[str] = None,
+    width: int = 1200,
+    height: int = 500,
 ):
-    fig = px.line(df, x="Datetime", y=target_col, title=title)
-    fig.update_layout(
-        xaxis_title="Date", yaxis_title=target_col, width=width, height=height
-    )
-    fig.show()
+
+    if title:
+        fig = px.line(df, x="Datetime", y=target_col, title=title)
+        fig.update_layout(
+            xaxis_title="Date", yaxis_title=target_col, width=width, height=height
+        )
+        fig.show()
+    else:
+        fig = px.line(df, x="Datetime", y=target_col)
+        fig.update_layout(
+            xaxis_title="Date", yaxis_title=target_col, width=width, height=height
+        )
+        fig.show()
 
 
 def time_series_outlier_plot(
